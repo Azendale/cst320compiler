a5e325c9f4a746579b70ec29a7adbeb819d1fc4f
Erik B. Andersen <erik.andersen@oit.edu>
2016-01-12 13:50:56 -0800

    Working on writing term function.

--- parse.cpp
+++ parse.cpp
@@ -28,6 +28,102 @@ bool FindPROG()
     }
     return true;
 }
+//*******************************************
+// Find a STMTS non-terminal
+bool FindSTMTS()
+{
+    while (FindSTMT())
+    {
+    }
+    return true;
+}
+//*******************************************
+// Find a STMT non-terminal
+bool FindSTMT()
+{
+    if (!FindEXPR()) return false;
+    int token = PeekToken();
+    if (';' != token)
+    {
+        Error("';'");
+        return false;
+    }
+    return true;
+}
+//*******************************************
+// Find a EXPR non-terminal
+bool FindEXPR()
+{
+    int token = PeekToken();
+    if ('(' == token) 
+    {
+        AdvanceToken();
+        if (!FindEXPR()) return false;
+        if (')' != PeekToken() )
+        {
+            Error("')'");
+            return false;
+        }
+        AdvanceToken();
+        if (!FindEXPR_P()) return false;
+        return true;
+    }
+    else if (!FindTERM()) return false; // Could have error here that they could have missed an open paren.
+    return true;
+}
+//*******************************************
+// Find a EXPR_P non-terminal
+bool FindEXPR_P()
+{
+    if (FindPLUSOP())
+    {
+        int token = PeekToken();
+        if ('(' != token )
+        {
+            Error("'('");
+            return false;
+        }
+        AdvanceToken(); // Advance past '('
+        if (!FindEXPR()) return false;
+        token = PeekToken();
+        if (')' != token)
+        {
+            Error("'('");
+            return false;
+        }
+        AdvanceToken(); // Advance past ')'
+        if (!FindEXPR_P()) return false;
+    }
+    return true; // Lamba production
+}
+//*******************************************
+// Find a TERM non-terminal
+bool FindTERM()
+{
+    int token = PeekToken();
+    if ('[' == token)
+    {
+        AdvanceToken(); // Move past '['
+        if (!FindEXPR()) return false;
+        token = PeekToken();
+        if (']' != token)
+        {
+            Error("']'");
+            return false;
+        }
+        AdvanceToken(); // Move past ']'
+        if (!FindTERM_P()) return false;
+        return true;
+    }
+    else if (NUM != token)
+    {
+        Error("a number");
+        return false;
+    }
+    return true;
+}
+
+
 /*******************************************
 bool FindExample()
 {
